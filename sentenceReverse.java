3e224fa00d2ab2f71fef22dfbcc69b17374269a8
logician-au <gbrunyee@hotmail.com>
2018-06-14 18:59:40 +0930

    A tidier version

--- sentenceReverse.java
+++ sentenceReverse.java
@@ -1,27 +1,17 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class sentenceReverse {
-
+	
 	public static String reverse(String str) {
-		StringBuilder sb = new StringBuilder(str);
-		StringBuilder sb2 = new StringBuilder();
-		List<String> wordArray = new ArrayList<>();
-		while (sb.length() > 0) {
-			int firstSpace = sb.indexOf(" ");
-			if (firstSpace > 0) {
-				String word = sb.substring(0, firstSpace);
-				wordArray.add(word);
-				sb = new StringBuilder(sb.substring(firstSpace + 1));
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0, j = 0; i < str.length(); i++) {
+			if (str.charAt(i) == ' ') {
+				sb.insert(0, ' ');
+				j = 0;
 			} else {
-				wordArray.add(sb.toString());
-				break;
+				sb.insert(j, str.charAt(i));
+				j++;
 			}
 		}
-		for (int i = wordArray.size() - 1; i >= 0; i--)
-			sb2.append(wordArray.get(i) + " ");
-		sb2 = new StringBuilder(sb2.substring(0,sb2.length()-1));
-		return sb2.toString();
+		return sb.toString();
 	}
 
 	public static void main(String[] args) {
