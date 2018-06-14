8927209a42e17c81c4945105ddd8d0bfe4887737
logician-au <gbrunyee@hotmail.com>
2018-06-14 21:22:50 +0930

    A better approach

--- sentenceReverse.java
+++ sentenceReverse.java
@@ -3,13 +3,10 @@ public class sentenceReverse {
 	public static String reverse(String str) {
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0, j = 0; i < str.length(); i++) {
-			if (str.charAt(i) == ' ') {
-				sb.insert(0, ' ');
-				j = 0;
-			} else {
-				sb.insert(j, str.charAt(i));
-				j++;
-			}
+			if (str.charAt(i) == ' ')
+				sb.insert(j=0, ' ');
+			else
+				sb.insert(j++, str.charAt(i));
 		}
 		return sb.toString();
 	}
